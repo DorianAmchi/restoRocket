ef29aa529b0bc30c71d44f1ef2927e96dc5dc7c4
Unknown <amchi.dorian@outlook.fr>
2018-05-16 17:19:52 +0200

    Fichier des Templates footer, header, modal, content, navbar

---
+++ template/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>NIKEMOUK</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" crossorigin="anonymous">
+    </head>
+    <body>
