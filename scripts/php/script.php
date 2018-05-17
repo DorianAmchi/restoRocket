b781053e092b1527067486609e99be73e0af9bc6
Unknown <amchi.dorian@outlook.fr>
2018-05-17 13:46:29 +0200

    Modif  included Script
    remove inscription script

--- scripts/php/script.php
+++ scripts/php/script.php
@@ -1,6 +1,5 @@
 <script type="text/javascript" src="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script> 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script type="text/javascript" src="/scripts/js/connexion.js"></script>
-<script type="text/javascript" src="/scripts/js/inscription.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
