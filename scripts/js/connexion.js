1ddf3a1386dfae08c91c748ef8a9da36f2bf5b65
Unknown <amchi.dorian@outlook.fr>
2018-05-17 13:35:06 +0200

    Modif Scripts
    Modif script Connexion.
    Ajout script traitement argument

--- scripts/js/connexion.js
+++ scripts/js/connexion.js
@@ -1,7 +1,40 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function logIn() {
+    var user = {
+        "username": $("#logUsername").val(),
+        "password": $("#logPassword").val()
+    }
 
+    $.ajax({
+        type: "POST",
+        url: "http://192.168.1.54:12108/verify",
+        data: user,
+        statusCode: {
+            200: function (user) {
+            },
+            401: function () {
+                alert('Erreur dans la saisie de vos identifiants.');
+            }
+        }
+    });
+}
 
+function signIn() {
+    var user = {
+        "username": $("#signUsername").val(),
+        "password": $("#signPassword").val()
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "http://192.168.1.54:12108/adduser",
+        data: user,
+        statusCode: {
+            200: function (user) {
+                
+            },
+            401: function () {
+                alert('Erreur dans la saisie de vos identifiants.');
+            }
+        }
+    });
+}
