0061a4c596346a7e637a7d96d75ec2d324b73669
Unknown <amchi.dorian@outlook.fr>
2018-05-20 21:07:46 +0200

    User Done

--- scripts/php/scripts.php
+++ scripts/php/scripts.php
@@ -1,12 +1,12 @@
 <script type="text/javascript" src="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script> 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <!--Script de connexion-->
-<script type="text/javascript" src="../js/connexion.js"></script>
+<script type="text/javascript" src="scripts/js/connexion.js"></script>
 <!--affectation des $_GET dans des $_SESSION et traitement des modals-->
 <?php
-include $_SERVER['DOCUMENT_ROOT'].'/scripts/php/scriptsArgument.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/scripts/php/scriptsArgument.php';
 ?>
 <!--Permet de clean l'url apres l'affectation des $_GET dans des $_SESSION grace au scriptsArguments.php-->
-<script type="text/javascript" src="../js/removeArg.js"></script>;;
+<!--<script type="text/javascript" src="/scripts/js/removeArg.js"></script>;;-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
