9d7c5967dcda4f3517d6da07cf470da548e14895
Unknown <amchi.dorian@outlook.fr>
2018-05-21 21:49:43 +0200

    Modif Page Carte

--- content/Cartes.php
+++ content/Cartes.php
@@ -1,17 +1,7 @@
 
-<div class='container col-md-auto page'>
-    <div class="row">
-        <img  class="img-fluid wallpaper" src= "/img/tableaunoir.jpg">
-        <h3 class="text-center txtCartes">Les Cartes</h3>
-    </div>
-    <div class='row justify-content-md-center gestionCarte'>
-        <div class='col text-center carte'>
-        </div>
-        <div class='col text-center cartes'>  
-            <form class="form-inline">
-                <button class="btn btn-outline-info my-2 my-sm-0" type="button" data-toggle="modal" data-target="#modalResto">Ajouter Un Restaurant</button>
-            </form>
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div id="restaurants" class="row">
         </div>
     </div>
 </div>
-
