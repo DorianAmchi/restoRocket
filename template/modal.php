c6426f3775b9c8b80fc34573ec3c09603d930f5d
Unknown <amchi.dorian@outlook.fr>
2018-05-21 21:47:09 +0200

    Modif Modal

--- template/modal.php
+++ template/modal.php
@@ -104,69 +104,70 @@ function connexionTRUE() {
     ?>
     <div class="modal-header">
         <h5 class="modal-title"><?php if (isset($_SESSION['username'])) {
-        echo$_SESSION['username'];} ?> !<br> Bienvenue sur Restologue.<br>Vous pouvez désormais profité de nos services.</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <?php
+        echo$_SESSION['username'];
     }
+    ?> !<br> Bienvenue sur Restologue.<br>Vous pouvez désormais profité de nos services.</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <?php
+}
 
-    function connexionFALSE() {
-        ?>
-        <div class="modal-header">
-            <h5 class="modal-title">Erreur lors de la saisie des identifiants.<br>Merci de bien vouloir réésayer.</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <input class="form-control mr-sm-2" id="logUsername" placeholder="Username" aria-label="Username">
-            <input class="form-control mr-sm-2" id="logPassword" placeholder="Password" aria-label="Password">
-        </div>
-        <div class="modal-footer">
-            <button class="btn btn-primary" type="submit" onclick="logIn()">Connexion</button>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        </div>
-        <?php
-    }
+function connexionFALSE() {
+    ?>
+    <div class="modal-header">
+        <h5 class="modal-title">Erreur lors de la saisie des identifiants.<br>Merci de bien vouloir réésayer.</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="modal-body">
+        <input class="form-control mr-sm-2" id="logUsername" placeholder="Username" aria-label="Username">
+        <input class="form-control mr-sm-2" id="logPassword" placeholder="Password" aria-label="Password">
+    </div>
+    <div class="modal-footer">
+        <button class="btn btn-primary" type="submit" onclick="logIn()">Connexion</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+    </div>
+    <?php
+}
 
-    function inscriptionTRUE() {
-        ?>
-        <div class="modal-header">
-            <h5 class="modal-title">Votre inscription à bien été validé.<br>Merci de vous connecter pour profité de nos service.</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <input class="form-control mr-sm-2" id="logUsername" placeholder="Username" aria-label="Username">
-            <input class="form-control mr-sm-2" id="logPassword" placeholder="Password" aria-label="Password">
-        </div>
-        <div class="modal-footer">
-            <button class="btn btn-primary" type="submit" onclick="logIn()">Connexion</button>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        </div>
-        <?php
-    }
+function inscriptionTRUE() {
+    ?>
+    <div class="modal-header">
+        <h5 class="modal-title">Votre inscription à bien été validé.<br>Merci de vous connecter pour profité de nos service.</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="modal-body">
+        <input class="form-control mr-sm-2" id="logUsername" placeholder="Username" aria-label="Username">
+        <input class="form-control mr-sm-2" id="logPassword" placeholder="Password" aria-label="Password">
+    </div>
+    <div class="modal-footer">
+        <button class="btn btn-primary" type="submit" onclick="logIn()">Connexion</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+    </div>
+    <?php
+}
 
-    function inscriptionFALSE() {
-        ?>
-        <div class="modal-header">
-            <h5 class="modal-title">L'identifiant est deja utilisé ou est invalide.<br> Merci de réessayer avec un nouveau.</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <input class="form-control mr-sm-2" id="signUsername" placeholder="Username" aria-label="Username">
-            <input class="form-control mr-sm-2" id="signPassword" placeholder="Password" aria-label="Password">
-            <input class = "form-control mr-sm-2" id = "signEmail" placeholder = "Email" aria-label = "Email">
-        </div>
-        <div class="modal-footer">
-            <button class="btn btn-primary" type="submit" onclick="signIn()">Inscription</button>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal" data-toggle="modal" data-target="#modalInscription">Close</button>
-        </div>
-        <?php
-    }
-    
+function inscriptionFALSE() {
+    ?>
+    <div class="modal-header">
+        <h5 class="modal-title">L'identifiant est deja utilisé ou est invalide.<br> Merci de réessayer avec un nouveau.</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="modal-body">
+        <input class="form-control mr-sm-2" id="signUsername" placeholder="Username" aria-label="Username">
+        <input class="form-control mr-sm-2" id="signPassword" placeholder="Password" aria-label="Password">
+        <input class = "form-control mr-sm-2" id = "signEmail" placeholder = "Email" aria-label = "Email">
+    </div>
+    <div class="modal-footer">
+        <button class="btn btn-primary" type="submit" onclick="signIn()">Inscription</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" data-toggle="modal" data-target="#modalInscription">Close</button>
+    </div>
+    <?php
+}
